ae7e986dc4604a23bcbffac6bbbd39506bf37da4
Bumi <bumi.iii001@gmail.com>
2021-05-16 23:11:13 -0400

    ping.js update

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(client, message, args, Discord){
-        message.channel.send('Pong!');
+        message.channel.send('pong ig');
         console.log("Command executed");
     }
 }
