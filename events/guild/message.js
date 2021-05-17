4d3e982b9c2b7fe244526c169a305c693bd1709a
Bumi <bumi.iii001@gmail.com>
2021-05-16 23:03:04 -0400

    second commit

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,5 +1,5 @@
 module.exports = (Discord, client, message) =>{
-    const prefix = 'hi';
+    const prefix = 'hey';
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
